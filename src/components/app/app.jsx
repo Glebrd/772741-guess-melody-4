0defd9c336521ea76c4d28caacfa7953f7b194dd
Gleb Dolgushin <dolgushing@gmail.com>
2020-06-13 20:36:41 +0300

    8. под покровом тестов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
+const welcomeButtonHandler = () => {};
+
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <WelcomeScreen errorsCount={errorsCount} onWelcomeButtonCLick={welcomeButtonHandler} />
   );
 };
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
-}
+};
 
 export default App;
---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import WelcomeScreen from "./welcome-screen";
+
+it(`WelcomeScreen should render correctly`, () => {
+  const tree = renderer
+    .create(
+        <WelcomeScreen errorsCount={3} onWelcomeButtonCLick={() => {
+        }}/>).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
